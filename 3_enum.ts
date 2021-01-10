f09b9e6f703cb02d72b69b76f96c6984694901ed
murash4 <singleant@gmail.com>
2021-01-10 23:52:57 +0300

    enum

---
+++ 3_enum.ts
@@ -0,0 +1,20 @@
+enum Membership {
+    Simple,
+    Standard,
+    Premium
+}
+
+const membership = Membership.Standard
+const membbershipRevers = Membership[2]
+
+console.log(membership)
+console.log(membbershipRevers)
+
+enum SocialMedia {
+    VK = 'VK',
+    FACEBOOK = 'FACEBOOK',
+    INSTAGRAM = 'INSTAGRAM'
+}
+
+const social = SocialMedia.INSTAGRAM
+console.log(social)
