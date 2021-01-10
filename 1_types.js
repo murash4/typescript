3508543d06865f5e6823584fbe892bba0b435f6e
murash4 <singleant@gmail.com>
2021-01-10 23:01:55 +0300

    data types

---
+++ 1_types.js
@@ -0,0 +1,20 @@
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var message = 'Hello Typesript';
+var numberArray = [1, 1, 2, 3, 5, 8, 13];
+var numberArray2 = [1, 1, 2, 3, 5, 8, 13];
+var words = ['hello', 'ts'];
+// Tuple
+var contact = ['vlad', 12347867];
+// Any
+var variable = 42;
+// ...
+variable = 'new string';
+variable = [];
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Фодр');
